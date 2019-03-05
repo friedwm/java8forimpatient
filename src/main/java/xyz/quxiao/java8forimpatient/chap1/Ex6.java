fc07bee2e1179e99587bbf7a7fade53aaf3321d2
quxiao <quxiao@meitunmama.com>
2019-03-05 12:32:16 +0800

    finish ex6

--- src/main/java/xyz/quxiao/java8forimpatient/chap1/Ex6.java
+++ src/main/java/xyz/quxiao/java8forimpatient/chap1/Ex6.java
@@ -1,28 +1,30 @@
 package xyz.quxiao.java8forimpatient.chap1;
 
 /**
- * @author 作者 :quxiao
- * 创建时间：2019/3/4 20:40
+ * @author 作者 :quxiao 创建时间：2019/3/4 20:40
  */
 public class Ex6 {
 
-    public static Runnable uncheck(RunnableEx runnableEx) {
-        try {
-            runnableEx.run();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+  public static Runnable uncheck(final RunnableEx runnableEx) {
+    return () -> {
+      try {
+        runnableEx.run();
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    };
+  }
 
 
-    public static void main(String[] args) {
-        new Thread(uncheck(() -> {
-            System.out.println("hello");
-            Thread.sleep(1000);
-        })).start();
-    }
+  public static void main(String[] args) {
+    new Thread(uncheck(() -> {
+      System.out.println("hello");
+      Thread.sleep(1000);
+    })).start();
+  }
 
-    public interface RunnableEx {
-        void run() throws Exception;
-    }
+  public interface RunnableEx {
+
+    void run() throws Exception;
+  }
 }
