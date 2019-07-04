a4c2b4cb7c4b5dbb2a35bc3c6b6e27a9789248d8
quxiao <quxiao@meitunmama.com>
2019-07-05 01:36:43 +0800

    测试Stream

--- src/main/java/xyz/quxiao/java8forimpatient/chap6/StreamInternal.java
+++ src/main/java/xyz/quxiao/java8forimpatient/chap6/StreamInternal.java
@@ -2,6 +2,7 @@ package xyz.quxiao.java8forimpatient.chap6;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author 作者 :quxiao 创建时间：2019/7/3 22:11
@@ -10,9 +11,9 @@ public class StreamInternal {
 
   public static void main(String[] args) {
     List<String> list = Arrays.asList("alpha", "beta", "charlie", "delta", "eagle");
-    long count = list.stream().filter(s -> {
-      return s.length() == 5;
-    }).count();
-    System.out.println(count);
+    Optional<Integer> first = list.stream().filter(s -> {
+      return s.length() > 1;
+    }).map(String::length).sorted().findFirst();
+    System.out.println(first.orElse(null));
   }
 }
